e994b7a34e620765eab2f1f7c0593241a69c2ea1
Olivier Barais <barais@irisa.fr>
2017-04-10 18:38:23 +0200

    update format

--- src/main/java/eu/stamp/dockerComposeParser/model/volume/Volumes.java
+++ src/main/java/eu/stamp/dockerComposeParser/model/volume/Volumes.java
@@ -11,35 +11,23 @@ public class Volumes {
 	private List<String> nameList = new ArrayList<String>();
 	private List<VolumeDescription> volumes = new ArrayList<VolumeDescription>();
 
-	
-
-
 	@JsonIgnore
 	public List<String> getNameList() {
 		return nameList;
 	}
 
-
-
-
 	public List<VolumeDescription> getVolumes() {
 		return volumes;
 	}
 
-
-
-
 	public void setVolumes(List<VolumeDescription> volumes) {
 		this.volumes = volumes;
 	}
-
-
-
 
 	@JsonAnySetter
 	public void setDynamicVolume(String name, VolumeDescription s) {
 		nameList.add(name);
 		volumes.add(s);
 	}
-	
+
 }
